3621c7bb487b19198f7560fd77e2c546bf0508bd
Stan <xuan9230@gmail.com>
2021-04-08 16:18:34 +1000

    Add validations & error handlings

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,34 +1,47 @@
 const { httpGet } = require("./mock-http-interface");
 
+/**
+ * Parse the response body to get the message
+ * @param {*} body
+ * @returns {string} message in the body
+ */
 const parseResBody = (body) => {
-  const { message } = JSON.parse(body);
-  return message || "";
+  try {
+    const { message } = JSON.parse(body);
+    return message || "";
+  } catch (err) {
+    return "Body must be an JSON object";
+  }
 };
 
 const getArnieQuotes = async (urls) => {
   const results = [];
 
-  await Promise.all(
-    urls.map(async (url) => {
-      try {
-        const { status, body } = await httpGet(url);
+  /**
+   * Get a single arnie quote with error handling
+   * @param {string} url
+   */
+  const getArnieQuote = async (url) => {
+    try {
+      const { status, body } = await httpGet(url);
 
-        if (status === 200) {
-          results.push({
-            "Arnie Quote": parseResBody(body),
-          });
-        } else {
-          results.push({
-            FAILURE: parseResBody(body),
-          });
-        }
-      } catch (err) {
+      if (status === 200) {
+        results.push({
+          "Arnie Quote": parseResBody(body),
+        });
+      } else {
         results.push({
           FAILURE: parseResBody(body),
         });
       }
-    })
-  );
+    } catch (err) {
+      results.push({
+        FAILURE: (err && err.message) || "",
+      });
+    }
+  };
+
+  await Promise.all(urls.map(getArnieQuote));
 
   return results;
 };
