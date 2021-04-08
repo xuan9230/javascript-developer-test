c778c7aae9c3c252503aa45c217d5cb0ba3868b5
Stan <xuan9230@gmail.com>
2021-04-08 14:58:53 +1000

    Implemented getArnieQuotes api

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,36 @@
-const { httpGet } = require('./mock-http-interface');
+const { httpGet } = require("./mock-http-interface");
+
+const parseResBody = (body) => {
+  const { message } = JSON.parse(body);
+  return message || "";
+};
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+
+  await Promise.all(
+    urls.map(async (url) => {
+      try {
+        const { status, body } = await httpGet(url);
+
+        if (status === 200) {
+          results.push({
+            "Arnie Quote": parseResBody(body),
+          });
+        } else {
+          results.push({
+            FAILURE: parseResBody(body),
+          });
+        }
+      } catch (err) {
+        results.push({
+          FAILURE: parseResBody(body),
+        });
+      }
+    })
+  );
+
+  return results;
 };
 
 module.exports = {
